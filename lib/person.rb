8d3b92ef1c53d5588740d1bbb9ab8a5eebc48be8
elizabethvera-atieno <atienolizvera@gmail.com>
2022-06-12 15:19:33 +0300

    commit tree cae308b056fdba4c02e94cb06bf85c5dcc5522f5

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,43 @@
-# your code goes here
+class Person 
+  attr_reader :name
+  attr_accessor :bank_account, :happiness, :hygiene
+
+  def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+    @name = name
+    @bank_account = bank_account
+    @happiness = happiness
+    @hygiene = hygiene
+
+  end
+  def hygiene
+    @hygiene.clamp(0, 10)
+  end
+  def happiness
+    @happiness.clamp(0, 10)
+  end
+
+  def happy?
+    self.happiness > 7
+  end
+
+  def clean?
+    self.hygiene > 7
+  end
+
+  def get_paid amount
+    @bank_account = self.bank_account + amount
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.happiness += 2
+    self.hygiene -= 3
+    "♪ another one bites the dust ♫"
+  end
+    
+end
